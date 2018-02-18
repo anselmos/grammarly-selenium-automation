f3654073e3bb53ae03fb1e9c0dd43602f729323a
Anselmos <anselmos@users.noreply.github.com>
2018-02-18 14:55:47 +0100

    Updates login and grammarly page. Login is now possible

--- grammarly_page.py
+++ grammarly_page.py
@@ -1,13 +1,16 @@
 from page_objects import PageObject, PageElement
-from selenium import webdriver
 
 class GrammarlyLogin(PageObject):
-    username = PageElement(id_='input[type="email"]')
+    username = PageElement(css='input[type="email"]')
     password = PageElement(css='input[type="password"]')
-    login = PageElement(css='input[type="submit"]')
+    login = PageElement(css='button[type="submit"]')
+    uri = 'https://www.grammarly.com'
+    signin_uri = '/signin'
 
     def make_login(self, username, password):
+        self.get(self.uri + self.signin_uri)
         self.username = username
         self.password = password
         self.login.click()
+
 
--- login.py
+++ login.py
@@ -9,21 +9,24 @@ class GrammarlyGeneralTest(unittest.TestCase):
     def setUp(self):
         # for dev I'll use only visible browser instead of no-gui one.
         # but for production, it will use the no-gui one.
-        self.driver = webdriver.Firefox()
+        # self.driver = webdriver.Firefox()
         # TODO uncomment it when finished coding.
-        # self.driver = webdriver.Remote(
-        #     command_executor='http://127.0.0.1:4444/wd/hub',
-        #     desired_capabilities={'browserName': 'firefox', 'javascriptEnabled': True}
-        # )
+        self.driver = webdriver.Remote(
+            command_executor='http://127.0.0.1:4444/wd/hub',
+            desired_capabilities={'browserName': 'firefox', 'javascriptEnabled': True}
+        )
+
 
     def test_grammarly_in_title(self):
-        self.driver.get("https://grammarly.com")
-        assert "Grammarly" in driver.title
+        page_login = GrammarlyLogin(self.driver)
+        self.driver.get(page_login.uri)
+        assert "Grammarly" in self.driver.title
 
     def test_login(self):
         page_login = GrammarlyLogin(self.driver)
         page_login.make_login('za2217279@mvrht.net', 'test123')
-        time.sleep(10)
+        time.sleep(2)
+        assert self.driver.current_url == "https://app.grammarly.com/"
 
     def tearDown(self):
         self.driver.close()
